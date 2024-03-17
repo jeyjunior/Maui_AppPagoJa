f64b5698d40b749fe1755950021ee17a101caaf2
José Junior <jeyjuniordg@gmail.com>
2024-03-17 13:54:13 -0300

    Update Layout
    - Definido o padrão dos componentes de vencimento
    - Definido o bind dos componentes
    - Definido eventos de exibição dos componentes

--- Views/RegistroBoleto.xaml.cs
+++ Views/RegistroBoleto.xaml.cs
@@ -6,4 +6,67 @@ public partial class RegistroBoleto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+		BindDiasVencimento();
+        BindMesVencimento();
+        BindAnoVencimento();
+        HabilitarComponentesVencimento();
+    }
+
+    private void BindDiasVencimento()
+    {
+        int[] dias = new int[31];
+
+        for (int i = 0; i < dias.Length; i++)
+        {
+            dias[i] = i + 1;
+        }
+
+        this.pckDias.ItemsSource = dias;
+        this.pckDias.SelectedIndex = 19;
+    }
+
+    private void BindMesVencimento()
+    {
+        int[] mes = new int[12];
+
+        for (int i = 0; i < mes.Length; i++)
+        {
+            mes[i] = i + 1;
+        }
+
+        this.pckMes.ItemsSource = mes;
+        this.pckMes.SelectedIndex = DateTime.Today.Month - 1;
+    }
+
+    private void BindAnoVencimento()
+    {
+        int anoAtual = DateTime.Today.Year;
+
+        int[] ano = new int[30];
+
+        for (int i = 0; i < ano.Length; i++)
+        {
+            ano[i] = anoAtual + i;
+        }
+
+        this.pckAno.ItemsSource = ano;
+        this.pckAno.SelectedIndex = 0;
+    }
+
+    private void chkPgtoContinuo_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        HabilitarComponentesVencimento();
+    }
+
+    private void HabilitarComponentesVencimento()
+    {
+        this.lblMes.IsEnabled = !this.chkPgtoContinuo.IsChecked;
+        this.pckMes.IsEnabled = !this.chkPgtoContinuo.IsChecked;
+
+        this.lblAno.IsEnabled = !this.chkPgtoContinuo.IsChecked;
+        this.pckAno.IsEnabled = !this.chkPgtoContinuo.IsChecked;
+    }
 }
